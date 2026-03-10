853fa1d511e144fc6429f742f62884d3aa03e1c0
Nishu177 <nsingh96427@gmail.com>
2026-03-10 14:12:01 +0530

    Update server.js

--- yogaproject/server.js
+++ yogaproject/server.js
@@ -7,7 +7,7 @@ const app = express();
 /* -------- Middleware -------- */
 app.use(cors());
 app.use(express.json());
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + "/nisha"));
 
 /* -------- MongoDB Atlas Connection -------- */
 // Make sure you have added your Atlas URI as Render Environment Variable named DATABASE_URL
@@ -54,6 +54,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
+
 
 
 
